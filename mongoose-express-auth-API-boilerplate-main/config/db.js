0d11a2e6580763d39174cc2f0a6e69b6295b483a
saparker <stephanieparker1997@gmail.com>
2022-12-02 02:57:11 -0500

    deploying

--- mongoose-express-auth-API-boilerplate-main/config/db.js
+++ mongoose-express-auth-API-boilerplate-main/config/db.js
@@ -2,7 +2,7 @@
 
 // creating a base name for the mongodb
 // REPLACE THE STRING WITH YOUR OWN DATABASE NAME
-const mongooseBaseName = 'Capstone-API'
+const mongooseBaseName = 'blogr-api'
 
 // create the mongodb uri for development and test
 const database = {
